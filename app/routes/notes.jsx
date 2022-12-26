dab6ef24c2b37a95d1ce09bbfa1dbc654062bd86
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2022-12-26 13:18:54 -0800

    Adding the backend of our project

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,4 +1,6 @@
+import { redirect } from "@remix-run/node";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
+import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function NotesPage() {
   return (
@@ -6,6 +8,22 @@ export default function NotesPage() {
       <NewNote />
     </main>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const noteData = Object.fromEntries(formData);
+  // const noteData = {
+  //   title: formData.get("title"),
+  //   content: formData.get("content"),
+  // };
+  // Add validation...
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+  // Redirecting the user
+  return redirect("/notes");
 }
 
 export function links() {

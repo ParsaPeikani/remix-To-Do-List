01884664585ab17565e2a900a882b92f543702bd
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2022-12-27 00:46:58 -0800

    Handling errors

--- app/root.jsx
+++ app/root.jsx
@@ -1,6 +1,7 @@
 import styles from "~/styles/main.css";
 
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -36,6 +37,32 @@ export default function App() {
   );
 }
 
+// export function ErrorBoundary({ error }) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <Meta />
+//         <Links />
+//         <title>An error occurred!</title>
+//       </head>
+//       <body>
+//         <header>
+//           <MainNavigation />
+//         </header>
+//         <main className="error">
+//           <h1>An error occurred!</h1>
+//           <p>{error.message}</p>
+//           <p>
+//             Back to <Link to="/">Saftey</Link>!
+//           </p>
+//         </main>
+//         <ScrollRestoration />
+//         <Scripts />
+//         <LiveReload />
+//       </body>
+//     </html>
+//   );
+// }
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
 }
